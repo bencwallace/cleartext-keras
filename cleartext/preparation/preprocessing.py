70466a6afc6bbf6381f7d6657b167cec0856f6be
Ben <bencwallace@gmail.com>
2020-06-02 11:39:21 -0400

    added preprocessing

---
+++ cleartext/preparation/preprocessing.py
@@ -0,0 +1,5 @@
+def preprocess(sentence):
+    sentence = re.sub(r'([?.!])', r' \1 ', sentence)
+    sentence = re.sub('\s{2,}', ' ', sentence)
+    sentence = sentence.strip()
+    return f'<START> {sentence} <END>'
