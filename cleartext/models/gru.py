7c0d9c1eccd56cb7a9001c06fe9fd4b0c03172a6
Ben <bencwallace@gmail.com>
2020-06-04 18:20:36 -0400

    implemented gru encoder decoder

---
+++ cleartext/models/gru.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+
+
+class EncoderDecoder(tf.keras.Model):
+    def __init__(self, vocab_size, embed_weights, units, seq_len):
+        super().__init__()
+        embed = layers.Embedding(vocab_size,
+                                 embed_weights.shape[1],
+                                 weights=[embed_weights],
+                                 input_length=seq_len,
+                                 trainable=False,
+                                 mask_zero=True)
+        self.encoder = Encoder(vocab_size, embed, units, seq_len)
+        self.decoder = Decoder(vocab_size, embed, units, seq_len)
+
+    def call(self, inputs, training=None):
+        # inputs is source word and target word (None if not training)
+        enc_in, dec_in = inputs
+        _, enc_state = self.encoder(enc_in)
+        dec_out = self.decoder([dec_in, enc_state], training)
+        return dec_out
+
+
+class Encoder(tf.keras.Model):
+    def __init__(self, vocab_size, embed_layer, units, seq_len):
+        super().__init__()
+        self.embed = embed_layer
+        self.gru = layers.GRU(units, return_state=True)
+
+    def call(self, inputs):
+        # inputs is source word
+        x = self.embed(inputs)
+        out, state = self.gru(x)
+        return out, state
+
+
+class Decoder(tf.keras.Model):
+    def __init__(self, vocab_size, embed_layer, units, seq_len):
+        super().__init__()
+        self.embed = embed_layer
+        self.gru = layers.GRU(units, return_sequences=True, return_state=True)
+        self.fc = layers.Dense(vocab_size)
+
+    def call(self, inputs, training=None):
+        # inputs is target word (None if not training) and encoder state
+        inputs, enc_state = inputs
+
+        if training:
+            x = self.embed(inputs)
+            out, _ = self.gru(x, initial_state=enc_state)
+            out = tf.reshape(out, (-1, out.shape[2]))
+            out = self.fc(out)
+            return out
+
+        raise NotImplementedError
--- cleartext/scripts/train.py
+++ cleartext/scripts/train.py
@@ -8,11 +8,12 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.preprocessing import text
 
 from ..models import build_lstm
+from ..models.gru import EncoderDecoder
 from ..preparation import load_embedding, load_data, prepare
 from ..utils import get_proj_root
 
 
-class LSTMTrainer(object):
+class Trainer(object):
     def load_data(self, vocab_size=10_000, dataset='wikismall', train_frac=0.9, num_examples='all'):
         self.vocab_size = vocab_size
         data = load_data(dataset, num_examples)
@@ -41,12 +42,31 @@ class LSTMTrainer(object):
         self.embed_dim = dim
         self.embed_matrix = load_embedding(dim, self.tokenizer, embedding)
 
+    def _left_shift(self, array):
+        zeros = np.zeros((array.shape[0], 1))
+        return np.hstack([array[:, 1:], zeros])
+
+    def _setup_callbacks(self):
+        proj_root = get_proj_root()
+        filename = '{epoch:02d}_loss_{loss:.4f}.hdf5'
+        filepath = os.path.join(proj_root, 'models', self._name, filename)
+        self.callbacks = [
+            EarlyStopping(patience=2),
+            ModelCheckpoint(filepath=filepath, save_best_only=True)
+        ]
+
+
+class LSTMTrainer(Trainer):
+    def __init__(self):
+        self._name = 'lstm'
+
     def build_model(self, units, dropout=0.5):
+        self.units = units
         self.model = build_lstm(self.vocab_size + 1, self.seq_len, units, self.embed_matrix)
         self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-        self._setup_callbacks(units)
 
     def train(self, epochs, batch_size=32, verbose=1, validation_split=0.1):
+        self._setup_callbacks()
         self.model.fit(x=[self.source_in, self.target_in],
                        y=self.target_out,
                        batch_size=batch_size,
@@ -55,14 +75,22 @@ class LSTMTrainer(object):
                        validation_split=validation_split,
                        callbacks=self.callbacks)
 
-    def _setup_callbacks(self, units):
-        proj_root = get_proj_root()
-        filepath = os.path.join(proj_root, 'models/lstm/lstm_epoch_{epoch:02d}_loss_{loss:.4f}.hdf5')
-        self.callbacks = [
-            EarlyStopping(patience=2),
-            ModelCheckpoint(filepath=filepath, save_best_only=True)
-        ]
 
-    def _left_shift(self, array):
-        zeros = np.zeros((array.shape[0], 1))
-        return np.hstack([array[:, 1:], zeros])
+class GRUTrainer(Trainer):
+    def __init__(self):
+        self._name = 'gru'
+
+    def build_model(self, units):
+        self.units = units
+        self.model = EncoderDecoder(self.vocab_size + 1, self.embed_matrix, units, self.seq_len)
+        self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+
+    def train(self, epochs, batch_size=32, verbose=1, validation_split=0.1):
+        self._setup_callbacks()
+        self.model.fit(x=[self.source_in, self.target_in],
+                       y=self.target_out,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       validation_split=validation_split,
+                       callbacks=self.callbacks)
